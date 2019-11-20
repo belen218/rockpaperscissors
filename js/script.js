97105d11a559a561f6943f3a2390096bcbe1498a
belen218 <belen.szz218@gmail.com>
2019-11-20 02:29:17 +0000

    added click handler

--- js/script.js
+++ js/script.js
@@ -5,6 +5,26 @@
 //GLOBAL VARIABLES
 /* global $ */
 
+var computerChoice
+var userChoice;
+var winner;
+var randomNumber = Math.random()
+
+$('#shoot').click(function() {
+    //get user input 
+    userChoice = $('#input').val()
+    $('#userChoice').text(userInput)
+    if (randomNumber > .666) {
+        computerChoice = 'rock'
+    } else if (randomNumber > .333){
+      computerChoice = 'scissors'  
+    }
+    else {
+        computerChoice = 'paper'
+    }
+
+
+})
 
 
 // DOCUMENT READY FUNCTION BELOW
